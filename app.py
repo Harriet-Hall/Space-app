3092091256753b7ce021876d289e2abf105741f7
Ivo-velev <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>
2025-10-05 18:27:20 +0100

    complete

--- app.py
+++ app.py
@@ -15,15 +15,30 @@ env = Environment(
 # asteroids = asteroid_dict["data"]
 # print(asteroids[1]["fullname"])
 
+def get_asteroid(asteroids, asteroid_name):
+    for asteroid in asteroids:
+        if asteroid["fullname"] == asteroid_name:
+            return asteroid
+        else:
+            continue
+
+
+
 def compute_crater_radius(asteroid):
     constant = 0.55
     gravity = 9.8
     asteroid_density = 3000
     earth_density = 2700
-    asteroid_diameter = asteroid["diameter"]
-    impact_velocity = asteroid["v_inf"]
-
-    crater_diameter = constant * (gravity **-0.22) *(asteroid_density/earth_density)**0.3*asteroid_diameter**0.78*impact_velocity**0.44
+    print(asteroid["diameter"])
+    if not asteroid["diameter"]:
+        asteroid_diameter = asteroid["diameter"]
+    else:
+        asteroid_diameter = 10
+    if not asteroid["v_inf"]:
+        impact_velocity = asteroid["v_inf"]
+    else:
+        impact_velocity = 30
+    crater_diameter = constant * (gravity **-0.22) *(asteroid_density/earth_density)**0.3*float(asteroid_diameter)**0.78*float(impact_velocity)**0.44
     crater_radius = crater_diameter / 2
     return crater_radius
 
@@ -32,15 +47,25 @@ def hello_world():
     if request.method == 'GET':
         if request.args.get("asteroid"):
             asteroid_name = request.args.get("asteroid")
-            print("This works!!!")
-        
+            print(asteroid_name)
+
+            response = requests.get("https://ssd-api.jpl.nasa.gov/sentry.api")
+            asteroid_dict = response.json()
+            asteroids = asteroid_dict["data"][:10]
+
+            asteroid = get_asteroid(asteroids, asteroid_name)
+            radius = compute_crater_radius(asteroid) * 1000
+
+            template = env.get_template('index.html')
+            return template.render(asteroids=asteroids, radius=radius)
+
     response = requests.get("https://ssd-api.jpl.nasa.gov/sentry.api")
     asteroid_dict = response.json()
     asteroids = asteroid_dict["data"][:10]
 
     template = env.get_template('index.html')
-    
     return template.render(asteroids=asteroids)
+        
 
 # @app.route("/asteroid?asteroid=<asteroid_name>")
 # def asteroid_name():
